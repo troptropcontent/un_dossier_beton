b5cfc385f427ee354417d779352574145b720567
Tom Ecrepont <tomecrepont@gmail.com>
2023-10-10 14:13:44 +0200

    add date_properties association to field and item

--- app/models/field.rb
+++ app/models/field.rb
@@ -1,5 +1,6 @@
 class Field < ApplicationRecord
   belongs_to :schema
+  has_many :date_properties, class_name: 'Properties::Date', dependent: :destroy
   enum kind: {
     date: 1
   }
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   belongs_to :folder
+  has_many :date_properties, class_name: 'Properties::Date', dependent: :destroy 
 end
